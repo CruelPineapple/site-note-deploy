6c709d504fc8027845e05f77d3a060ca58460fe0
ssandra <1617218599@qq.com>
2021-09-13 09:48:03 +0800

    auto deploy

--- d.sh
+++ d.sh
@@ -1,7 +1,4 @@
 git add *
 git commit -m "auto deploy"
 git push
-ssh root@sakurajimama1.ltd
-cd ..
-cd usr/local/nginx/html/site-note-deploy
-git pull
+ssh root@sakurajimama1.ltd "cd ..;cd usr/local/nginx/html/site-note-deploy; git pull"
